905733fcbaff1df783da0c75a7c0d44d058af3a7
Mykola Batrak <kolyaun@live.com>
2023-08-28 17:44:26 +0300

    fixed component select

--- client/src/components/select/Select.jsx
+++ client/src/components/select/Select.jsx
@@ -1,19 +1,37 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getProductsArray } from "../../redux/actions/merchandise";
 import "./_select.scss";
 
-const Select = () => (
-  <div>
-    <label className="select__label" htmlFor="sort">
-      Сортування:
-    </label>
-    <select name="sort" id="sort">
-      <option value="null">Інше</option>
-      <option value="minPrice">Від дешевих</option>
-      <option value="maxPrice">Від дорогих</option>
-    </select>
-  </div>
-);
+const Select = () => {
+  const [sort, setSort] = useState(null);
+  const { minPrice, maxPrice, page, grade, roasting, brand, type } = useSelector(
+    (state) => state.merchandise
+  );
+  const dispatch = useDispatch();
+
+  const handleChange = (event) => {
+    setSort(event.target.value);
+  };
+
+  useEffect(() => {
+    dispatch(getProductsArray(page, sort, minPrice, maxPrice, grade, roasting, brand, type));
+  }, [dispatch, sort]);
+
+  return (
+    <div>
+      <label className="select__label" htmlFor="sort">
+        Сортування:
+      </label>
+      <select name="sort" id="sort" onChange={handleChange}>
+        <option value="null">Інше</option>
+        <option value="+">Від дешевих</option>
+        <option value="-">Від дорогих</option>
+      </select>
+    </div>
+  );
+};
 
 export default Select;
